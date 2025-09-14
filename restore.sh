262a92f5ddf889d481c9282d52bae62f56755e45
Velox <161338819+VeloxNF@users.noreply.github.com>
2025-09-14 18:05:10 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,27 +1,27 @@
-echo "=== [1/9] #Enter Maintenance Mode ==="
+echo "=== [1/8] #Enter Maintenance Mode ==="
 php artisan down
 
-echo "=== [2/9] #Download the Update ==="
+echo "=== [2/8] #Download the Update ==="
 curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
 chmod -R 755 storage/* bootstrap/cache
 
-echo "=== [4/9] #Update Dependencies ==="
+echo "=== [3/8] #Update Dependencies ==="
 composer install --no-dev --optimize-autoloader
 
-echo "=== [5/9] #Clear Compiled Template Cache ==="
+echo "=== [4/8] #Clear Compiled Template Cache ==="
 php artisan view:clear
 php artisan config:clear
 
-echo "=== [6/9] #Database Updates ==="
+echo "=== [5/8] #Database Updates ==="
 php artisan migrate --seed --force
 
-echo "=== [7/9] #Set Permissions ==="
+echo "=== [6/8] #Set Permissions ==="
 chown -R www-data:www-data /var/www/pterodactyl/*
 
-echo "=== [8/9] #Restarting Queue Workers ==="
+echo "=== [7/8] #Restarting Queue Workers ==="
 php artisan queue:restart
 
-echo "=== [9/9] #Exit Maintenance Mode ==="
+echo "=== [8/8] #Exit Maintenance Mode ==="
 php artisan up
 
 echo "=== ALL DONE ==="
